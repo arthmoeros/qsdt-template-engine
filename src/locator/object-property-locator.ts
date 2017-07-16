94ccac1a2f5a7980721620e805e61d9b7ac20b6f
arthmoeros <artu.saavedra@gmail.com>
2017-07-16 00:56:23 -0400

    in dev

---
+++ src/locator/object-property-locator.ts
@@ -0,0 +1,39 @@
+/**
+ * @class ObjectPropertyLocator
+ * @author Arturo Saavedra (arthmoeros)
+ * 
+ * This locator is used to determine a property value within an object
+ */
+export class ObjectPropertyLocator {
+
+    /**
+     * Looks up a value within a object given a path
+     * 
+     * @param object object to inspect
+     * @param path Path to lookup in the object must follow the format root.parent.children
+     * @throws an Error if the path points to a member of an array
+     */
+    public static lookup(object: any, path: string): any {
+        let nesting: string[] = path.split(".");
+        let resolvedValue: any = null;
+        let nextChild: any = object;
+        if(nextChild == null){
+            throw new Error("Root object is null");
+        }
+        for(let i = 0; i < nesting.length; i++){
+            nextChild = nextChild[nesting[i]];
+            if (nextChild == null) {
+                resolvedValue = null;
+                break;
+            }else{
+                if((i-1) < nesting.length && Array.isArray(nextChild)){
+                    throw new Error(`Path points to an array's child: ${path} at ${nesting[i+1]}`);
+                }
+                if((i+1) == nesting.length){
+                    resolvedValue = nextChild;
+                }
+            }
+        }
+        return resolvedValue;
+    }
+}
---
+++ src/locator/sub-template-locator.ts
@@ -0,0 +1,15 @@
+export class SubTemplateLocator{
+
+
+    
+}
+
+class SubTemplate{
+
+    private startIndex: number;
+    private endIndex: number;
+    private type: string;
+    private expression: string;
+
+    private children: SubTemplate[];
+}
